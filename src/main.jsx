0811d44864e678ce5f9ee54257ec606ef1a93cf5
Sodiqjonmukhtorov <sodiqjonm16@gmail.com>
2024-09-07 15:13:31 +0500

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client'; // createRoot import qilinmoqda
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import App from './App.jsx';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import { createRoot } from "react-dom/client"; // createRoot import qilinmoqda
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 const queryClient = new QueryClient();
-const root = createRoot(document.getElementById('root')); // createRoot to'g'ri chaqirildi
+const root = createRoot(document.getElementById("root")); // createRoot to'g'ri chaqirildi
 
 root.render(
   <BrowserRouter>
