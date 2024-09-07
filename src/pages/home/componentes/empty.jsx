f1edca36d0aaa18b040d528b71c46fb8ad0ab399
Muxsinjon1610 <muxsinjon1610@gmail.com>
2024-09-07 14:06:41 +0500

    ok

---
+++ src/pages/home/componentes/empty.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const empty = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti iure
+      veritatis magnam illo. Ducimus nisi, ut quia magnam in at autem
+      consequatur laboriosam quo tempore, sequi culpa! Ab, cumque non.
+    </div>
+  );
+};
+
+export default empty;
