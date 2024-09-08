5446237558619589fd20fc325980acc9416548e5
Muxsinjon1610 <muxsinjon1610@gmail.com>
2024-09-08 09:49:56 +0500

    amount finished

--- src/pages/home/components/amout/amout.jsx
+++ src/pages/home/components/amout/amout.jsx
@@ -1,7 +1,63 @@
 import React from "react";
+import CountUp from "react-countup";
 
 export const Amount = () => {
-  return <div>{/* Tarkib */}</div>;
+  return (
+    <div className="container my-10">
+      <div className="flex items-center flex-wrap justify-evenly">
+        <div className="bg-amountBg pt-14 text-center w-[135px] h-[160px] bg-no-repeat">
+          <CountUp
+            end={15}
+            suffix="+"
+            className="text-white font-secondFamily font-semibold text-2xl"
+          />
+          <p className="text-sm text-center mt-10 text-blue-900 font-semibold">
+            Years of experience
+          </p>
+        </div>
+        <div className="bg-amountBg pt-14 text-center w-[135px] h-[160px] bg-no-repeat">
+          <CountUp
+            end={300}
+            suffix="+"
+            className="text-white font-secondFamily font-semibold text-2xl"
+          />
+          <p className="text-sm text-center mt-10 text-blue-900 font-semibold">
+            Students
+          </p>
+        </div>
+        <div className="bg-amountBg pt-14 text-center w-[135px] h-[160px] bg-no-repeat">
+          <CountUp
+            end={1}
+            suffix="M+"
+            className="text-white font-secondFamily font-semibold text-2xl"
+          />
+          <p className="text-sm text-center mt-10 text-blue-900 font-semibold">
+            Views
+          </p>
+        </div>
+        <div className="bg-amountBg pt-14 text-center w-[135px] h-[160px] bg-no-repeat">
+          <CountUp
+            end={100}
+            suffix="K+"
+            className="text-white font-secondFamily font-semibold text-2xl"
+          />
+          <p className="text-sm text-center mt-10 text-blue-900 font-semibold">
+            Students worldwide
+          </p>
+        </div>
+        <div className="bg-amountBg pt-14 text-center w-[135px] h-[160px] bg-no-repeat">
+          <CountUp
+            end={10}
+            suffix="+"
+            className="text-white font-secondFamily font-semibold text-2xl"
+          />
+          <p className="text-sm text-center mt-10 text-blue-900 font-semibold">
+            Courses
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Amount; // Bu yerda default export borligini ta'minlang
+export default Amount;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        amountBg: "url('amountBg.svg')",
+      },
       fontFamily: {
         fontFamily: ["Manrope", "sans-serif"],
         secondFamily: ["Lobster", "cursive"],
