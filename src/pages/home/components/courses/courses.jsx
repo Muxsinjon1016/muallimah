e8e2dd4a3fbfb591996a333757a74a5c5243344a
Sodiqjonmukhtorov <sodiqjonm16@gmail.com>
2024-09-08 09:37:20 +0500

    ok

--- src/pages/home/components/courses/courses.jsx
+++ src/pages/home/components/courses/courses.jsx
@@ -3,7 +3,35 @@ import React from "react";
 export const Courses = () => {
   return (
     <>
-
+      <div>
+        <ul>
+          <li>
+            <p>
+              <a href="/"></a>
+            </p>
+          </li>
+          <li>
+            <p>
+              <a href="/"></a>
+            </p>
+          </li>
+          <li>
+            <p>
+              <a href="/"></a>
+            </p>
+          </li>
+          <li>
+            <p>
+              <a href="/"></a>
+            </p>
+          </li>
+          <li>
+            <p>
+              <a href="/"></a>
+            </p>
+          </li>
+        </ul>
+      </div>
     </>
   );
 };
